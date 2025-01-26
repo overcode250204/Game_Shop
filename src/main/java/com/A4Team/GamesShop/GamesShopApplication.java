450ad6e3709b85e1c442b1289393fba923de3141
overcode250204 <saoran123456@gmail.com>
2025-01-26 12:45:30 +0700

    FirstCommit

---
+++ src/main/java/com/A4Team/GamesShop/GamesShopApplication.java
@@ -0,0 +1,13 @@
+package com.A4Team.GamesShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/A4Team/GamesShop/GamesShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.A4Team.GamesShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
