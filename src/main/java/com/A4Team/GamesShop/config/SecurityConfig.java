3460be36df6844a4f855ea267de0a6ac8e799f88
LE_THANH_TRONG <lethanhtrong161005@gmail.com>
2025-01-31 11:42:52 +0700

    config-project

---
+++ src/main/java/com/A4Team/GamesShop/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.A4Team.GamesShop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+}
